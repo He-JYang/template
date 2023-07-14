09f126795eb33e1a99c3bb8337067e4ddda0d341
He-JYang <121350342+He-JYang@users.noreply.github.com>
2023-07-14 10:53:03 +0800

    change

--- 字符串/马拉车算法.cpp
+++ 字符串/马拉车算法.cpp
@@ -3,60 +3,48 @@
 // 马拉车算法 Manacher‘s Algorithm 是用来查找一个字符串的最长回文子串的线性方法
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-
-
-
-
-
-string preProcess(string s) {
-    int n = s.length();
-    if (n == 0) { return "^$"; }
-    string ret = "^";
-    for (int i = 0; i < n; i++)
-        ret += "#" + s.charAt(i);
-    ret += "#$";
-    return ret;
-}
-
 // 马拉车算法
+const int maxn = 1e6 + 10;
+string temp;
+int p[maxn];
 
-string longestPalindrome2(string s) {
-    string T = preProcess(s);
-    int n = T.length();
-    int[] P = new int[n];
+string longestPalindrome2(string& s) {
+    temp = "^";
+    for (auto ch : s) {
+        temp += "#" + ch;
+    }
+    if (s.length()) temp += "#";
+    temp += "$";
+
+    int n = temp.length();
     int C = 0, R = 0;
     for (int i = 1; i < n - 1; i++) {
         int i_mirror = 2 * C - i;
-        if (R > i) {
-            P[i] = min(R - i, P[i_mirror]);  // 防止超出 R
-        } else {
-            P[i] = 0;  // 等于 R 的情况
-        }
+        p[i] = min(R - i, p[i_mirror]);  // 防止超出 R
 
         // 碰到之前讲的三种情况时候，需要利用中心扩展法
-        while (T.charAt(i + 1 + P[i]) == T.charAt(i - 1 - P[i])) {
-            P[i]++;
+        while (temp[i + 1 + p[i]] == temp[i - 1 - p[i]]) {
+            p[i]++;
         }
 
         // 判断是否需要更新 R
-        if (i + P[i] > R) {
+        if (i + p[i] > R) {
             C = i;
-            R = i + P[i];
+            R = i + p[i];
         }
     }
 
-    // 找出 P 的最大值
+    // 找出 p 的最大值
     int maxLen = 0;
     int centerIndex = 0;
     for (int i = 1; i < n - 1; i++) {
-        if (P[i] > maxLen) {
-            maxLen = P[i];
+        if (p[i] > maxLen) {
+            maxLen = p[i];
             centerIndex = i;
         }
     }
     int start = (centerIndex - maxLen) / 2;  // 最开始讲的求原字符串下标
-    return s.substring(start, start + maxLen);
+    return s.substr(start, start + maxLen);
 }
